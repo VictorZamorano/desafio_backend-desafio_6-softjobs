f1bc900dbbfe2b7f208785c665935d75d7abce20
Victor <vzamoranoguerra@gmail.com>
2023-06-14 19:29:56 -0400

    morgan install

--- Backend/index.js
+++ Backend/index.js
@@ -3,12 +3,14 @@ dotenv.config();
 
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 import myRouter from "./routes/routes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.use("/", myRouter);
 
